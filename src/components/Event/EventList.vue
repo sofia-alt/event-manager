1c3295c8ce567250b4829b44f8fcf1daa8b04390
Sofia Abaeva <medi61200@mail.ru>
2022-01-14 13:49:59 +0300

    scroll

--- src/components/Event/EventList.vue
+++ src/components/Event/EventList.vue
@@ -1,12 +1,17 @@
 <template>
   <v-layout>
     <v-card class="event__container">
-      <v-list class="event__list">
-        <v-list-item
-          v-for="item in events"
-          :key="item.ID"
-        >
-          <router-link :to="{ name: 'event', params: { id: item.ID }}" class="event__link">
+      <v-list
+        class="event__list"
+        v-infinite-scroll="loadEvents"
+        infinite-scroll-disabled="busy"
+        infinite-scroll-distance="10"
+      >
+        <v-list-item v-for="item in events" :key="item.ID">
+          <router-link
+            :to="{ name: 'event', params: { id: item.ID } }"
+            class="event__link"
+          >
             <event-date :date-event="item.From"></event-date>
             {{ item.Name }}
           </router-link>
@@ -32,6 +37,10 @@
     data() {
       return {}
     },
-    created() {},
-  })
+    methods: {
+      loadEvents() {
+        console.log('Запрос на сервер')
+      }
+    }
+})
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import infiniteScroll from 'vue-infinite-scroll'
+
 Vue.config.productionTip = false
+Vue.use(infiniteScroll)
 
 require('@/assets/main.scss')
 new Vue({
